3b7467f9eb1cf811e01a8df505e11cda198827b2
Connector403 <hafiz-2010@hotmail.co.uk>
2021-08-09 17:09:51 +0100

    Navbar completly Sorted out animation finally working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import News from "./pages/News";
+
+import {
+  BrowserRouter as Router,
+  Route,
+
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+
+const  App =() => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about" exact>
+              <About />
+            </Route>
+            <Route path="/news" exact>
+              <News />
+            </Route>
+            <Route path="/contact" exact>
+              <Contact />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
